c3986dca9dba6f9403b013f7a1017b153e30fc89
filippaska <filippaska2005@gmail.com>
2023-12-01 07:33:05 -0500

    Dodalem userID oraz zmienilem nazwy zmiennych

--- backend/notes/session.py
+++ backend/notes/session.py
@@ -1,14 +1,20 @@
 class sesja(object):
-    def GetUserUsername(self, request):
-        userName = request.session.get('userName', None)
+    def GetUsername(self, request):
+        userName = request.session.get('username', None)
         return userName
 
-    def GetUserPassword(self, request):
-        userPassword = request.session.get('userPassword', None)
-        return userPassword
+    def GetPassword(self, request):
+        password = request.session.get('password', None)
+        return password
 
-    def PostUsername(self, request, userName):
-        request.session['userName'] = userName
+    def GetUserId(self, request):
+        userID = request.session.get('userID', None)
 
-    def PostUserPassword(self, request, userPassword):
-        request.session['userPassword'] = userPassword
+    def PostUsername(self, request, username):
+        request.session['username'] = username
+
+    def PostPassword(self, request, password):
+        request.session['password'] = password
+    
+    def PostUserID(self, request, userID):
+        request.session['userID'] = userID
